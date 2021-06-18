1678ddf55e20681befd2b0d8f391bff49f91f8da
bhavikx <64426653+bhavikx@users.noreply.github.com>
2021-06-18 15:10:21 +0530

    Update settings.py

--- post/post/settings.py
+++ post/post/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@10hutce8o7x0qd(&@a384l4v5z!h483kb+(8ez2gnchsz61zi'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

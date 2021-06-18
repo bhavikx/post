dc1df3c8ec1a431bd7e6168ff265e6a4cf2275fa
bhavikx <64426653+bhavikx@users.noreply.github.com>
2021-06-18 15:19:07 +0530

    Update models.py

--- post/like/models.py
+++ post/like/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Post(models.Model):
-    post_text = models.TextField(max_length=50)
+    post_text = models.TextField(max_length=45)
     post_img = models.ImageField(upload_to="images/", null=True, blank=True)
     like = models.BooleanField(default=False)
 
